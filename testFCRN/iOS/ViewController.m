7d0843781587717f4a81aaea18ece9d2478eb7b8
Doron Adler <doron@glidetalk.com>
2019-11-04 19:10:51 +0200

    Added (bad looking) UI for iOS

--- testFCRN/iOS/ViewController.m
+++ testFCRN/iOS/ViewController.m
@@ -30,13 +30,23 @@
 
 @property (nonatomic, strong) ImagePlatform* imagePlatform;
 
+@property (nonatomic, weak) IBOutlet UIImageView *inputImageView;
 @property (nonatomic, strong) NSString *mediaType;
 @property (nonatomic, strong) UIImage *croppedInputImage;
+@property (nonatomic, weak) IBOutlet UIImageView *croppedInputImageView;
 
 @property (nonatomic, strong) UIImage *disparityImage;
+@property (nonatomic, weak) IBOutlet UIImageView *disparityImageImageView;
+
 @property (nonatomic, strong) UIImage *depthHistogramImage;
+@property (nonatomic, weak) IBOutlet UIImageView *depthHistogramImageImageView;
+
+@property (nonatomic, weak) IBOutlet UILabel *statusLabel;
 
 @property (nonatomic, strong) NSData *combinedImageData;
+
+@property (nonatomic, weak) IBOutlet UIButton *imageOpenButton;
+@property (nonatomic, weak) IBOutlet UIButton *depthImageSaveButton;
 
 @end
 
@@ -47,11 +57,9 @@
     
     self.imagePlatform = [[ImagePlatform alloc] init];
     
-//    self.textView.stringValue = NSLocalizedString(@"depthPrediction.loading", @"Loading model");
-//    self.imageOpenButton.enabled = NO;
-//    self.aspectFillImageSaveButton.enabled = NO;
-//    self.depthImageSaveButton.enabled = NO;
-//    self.combinedImageSaveButton.enabled = NO;
+    [self updateStatusLabelText:@"Loading model"];
+    self.imageOpenButton.enabled = NO;
+    self.depthImageSaveButton.enabled = NO;
        
     dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), ^{
         [self loadModel];
@@ -76,9 +84,10 @@
 
 - (void)didLoadModel {
     //self.textView.stringValue = NSLocalizedString(@"depthPrediction.readyToOpen", @"Please open an image");
-    //self.imageOpenButton.enabled = YES;
     NSLog(@"didLoadModel (\"%@\")", ML_MODEL_CLASS_NAME_STRING);
+    [self updateStatusLabelText:[NSString stringWithFormat:@"didLoadModel (\"%@\")", ML_MODEL_CLASS_NAME_STRING]];
     dispatch_async(dispatch_get_main_queue(), ^{
+        self.imageOpenButton.enabled = NO;
         [self openImagePickerAndSelectImage];
     });
     
@@ -87,6 +96,31 @@
 - (void)didFailToLoadModelWithError:(NSError*)error {
     //self.textView.stringValue = NSLocalizedString(@"depthPrediction.failToLoad", @"Error loading model");
     NSLog(@"Error loading model (\"%@\") because \"%@\"", ML_MODEL_CLASS_NAME_STRING, error);
+    [self updateStatusLabelText:[NSString stringWithFormat:@"Error loading model (\"%@\") because \"%@\"", ML_MODEL_CLASS_NAME_STRING, error.localizedDescription]];
+}
+
+#pragma mark - Status label
+
+- (void)updateStatusLabelText:(NSString*)text {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.statusLabel.text = text;
+    });
+}
+
+#pragma mark - Button action handlers
+
+- (IBAction)handleActionForImageOpenButton:(id)sender {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.imageOpenButton.enabled = NO;
+        [self openImagePickerAndSelectImage];
+    });
+}
+
+- (IBAction)handleActionForDepthImageSaveButton:(id)sender {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.depthImageSaveButton.enabled = NO;
+        UIImageWriteToSavedPhotosAlbum(self.disparityImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+    });
 }
 
 #pragma mark - UIImagePickerController
@@ -108,17 +142,33 @@
         
         self.mediaType = [info objectForKey:UIImagePickerControllerMediaType];
         if (inputImage) {
-            [self predictDepthMapFromInputImage:inputImage];
+            self.inputImageView.image = inputImage;
+            [self dismissViewControllerAnimated:YES completion:^{
+                self.imageOpenButton.enabled = NO;
+                self.depthImageSaveButton.enabled = NO;
+               [self predictDepthMapFromInputImage:inputImage];
+            }];
+            
         }
+        
+        
        });
 }
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-    
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    [self dismissViewControllerAnimated:NO completion:^{
+       self.imageOpenButton.enabled = YES;
+    }];
 }
 
 // Adds a photo to the saved photos album.  The optional completionSelector should have the form:
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
     NSLog(@"image: %@ didFinishSavingWithError: %@ contextInfo: 0x%llx", image, error, (uint64_t)contextInfo);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.depthImageSaveButton setEnabled:YES];
+        NSLog(@"Depth image was saved to gallery");
+        [self updateStatusLabelText:@"Depth image was saved to gallery"];
+    });
 }
 
 
@@ -132,6 +182,7 @@
          dispatch_async(dispatch_get_main_queue(), ^{
             // self.textView.stringValue = NSLocalizedString(@"depthPrediction.completionHandler", @"Processing results...");
              NSLog(@"Processing results...");
+             [self updateStatusLabelText:@"Processing results..."];
          });
         NSArray *results = request.results;
         NSLog(@"results = \"%@\"", results);
@@ -182,11 +233,14 @@
     self.handler = [[VNImageRequestHandler alloc] initWithCGImage: imageRef
                                                           options:@{VNImageOptionCIContext : self.imagePlatform.imagePlatformCoreContext}];
     //[self.handler performRequests:self.request];
+    [self updateStatusLabelText:@"Predicting depth map..."];
     [self.handler performRequests:@[self.request] error:&error];
 }
 
 - (void)didFinish {
-    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.imageOpenButton.enabled = YES;
+    });
 }
 
 - (void)didPrepareImages {
@@ -197,6 +251,7 @@
         
         //self.textView.stringValue = NSLocalizedString(@"depthPrediction.didPrepareImages", @"Images are ready");
         NSLog(@"Images are ready");
+        [self updateStatusLabelText:@"Images are ready"];
         
         [PHPhotoLibrary requestAuthorization:^( PHAuthorizationStatus status ) {
             if ( status == PHAuthorizationStatusAuthorized ) {
@@ -204,6 +259,7 @@
                    
                     if (self.combinedImageData) {
                         NSLog(@"Combined image data is available");
+                        [self updateStatusLabelText:@"Portrait image data is available"];
                         PHAssetResourceCreationOptions* options = [[PHAssetResourceCreationOptions alloc] init];
                         options.uniformTypeIdentifier =  self.mediaType;
                         PHAssetCreationRequest* creationRequest = [PHAssetCreationRequest creationRequestForAsset];
@@ -213,41 +269,36 @@
                 } completionHandler:^( BOOL success, NSError* _Nullable error ) {
                     if ( ! success ) {
                         NSLog( @"Error occurred while saving photo to photo library: %@", error );
+                        [self updateStatusLabelText:[NSString stringWithFormat:@"Error occurred while saving photo to photo library: %@", error.localizedDescription]];
+                    } else {
+                        [self updateStatusLabelText:@"Cropped portrait image was saved to gallery"];
                     }
                     
                     [self didFinish];
                 }];
             }
             else {
-                NSLog( @"Not authorized to save photo" );
+                NSLog(@"Not authorized to save photo" );
+                [self updateStatusLabelText:@"Not authorized to save photo"];
                 [self didFinish];
             }
         }];
                     
         if (self.disparityImage) {
-//            [self.depthImageView setContentsGravity:kCAGravityResizeAspect];
-//            [self.depthImageView setImage:self.disparityImage];
-//            [self.depthImageSaveButton setEnabled:YES];
-            //UIImageWriteToSavedPhotosAlbum(self.disparityImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+            [self.disparityImageImageView setContentMode:UIViewContentModeScaleAspectFit];
+            [self.disparityImageImageView setImage:self.disparityImage];
+            [self.depthImageSaveButton setEnabled:YES];
         }
         
         if (self.croppedInputImage) {
-//            [self.imageView setContentsGravity:kCAGravityResizeAspect];
-//            [self.imageView setImage:self.croppedInputImage];
-           // UIImageWriteToSavedPhotosAlbum(self.croppedInputImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+            [self.croppedInputImageView setContentMode:UIViewContentModeScaleAspectFit];
+            [self.croppedInputImageView setImage:self.croppedInputImage];
         }
         
         if (self.depthHistogramImage) {
-            //[self.histogramImageView setImage:self.depthHistogramImage];
+            [self.depthHistogramImageImageView setImage:self.depthHistogramImage];
         }
         
-        if (self.combinedImageData) {
-            //[self.combinedImageSaveButton setEnabled:YES];
-            //UIImage *combinedImage = [UIImage imageWithData:self.combinedImageData];
-            //UIImageWriteToSavedPhotosAlbum(combinedImage, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-        }
-        
-        //self.imageOpenButton.enabled = YES;
     });
 }
 
